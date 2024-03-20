fa3aab611012d9b96ca8ec6ba4d45a011d8fd040
otreborBz <rlcarval38@gmail.com>
2024-03-20 20:05:54 -0300

    app finalizado

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,79 +1,82 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, FlatList, SafeAreaView, Alert } from 'react-native';
-import Styles from './style/';
+import { View, Text, TextInput, TouchableOpacity, FlatList, Keyboard } from 'react-native';
 import ListaPaises from '../paises/listaPaises.js';
 import api from '../service/api.js';
+import styles from './style/index.js'
 
 export default function Form() {
+  const [paises, setPaises] = useState([]);
+  const [nomePais, setNomePais] = useState('');
+  const [capitalPais, setCapitalPais] = useState('');
 
-    const [paises, setPaises] = useState([]);
-    const [nomePais, setNomePais] = useState('');
-    const [capitalPais, setCapitalPais] = useState('');
+  useEffect(() =>{
+    atualizaLista();
+  }, []);
 
+  function atualizaLista(){
+    api.get('/pais')
+      .then((response) => {
+        setPaises(response.data);
+      })
+      .catch((error) => {
+        console.error('Erro na requisição:', error);
+      });
+  }
 
-    // Carrega a lista de paises no banco
-    useEffect(() =>{
-        api.get('/pais')
-            .then((response) => {
-                setPaises(response.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
+  function adicionar(){
+    const novoPais = {
+      nome: nomePais,
+      capital: capitalPais
+    };
 
-    function adicionar(){
-        const novoPais = {
-            nome: nomePais,
-            capital: capitalPais
-        };
+    api.post('/pais', novoPais)
+      .then((response) => {
+        setPaises([...paises, response.data]);
+        setNomePais('');
+        setCapitalPais('');
+      })
+      .catch((error) => {
+        console.error('Erro ao adicionar:', error);
+      }).finally(()=>{
+        Keyboard.dismiss();
+      })
+  }
 
-        api.post('/pais', novoPais)
-            .then((response) => {
-                setPaises([...paises, response.data]);
-                setNomePais('');
-                setCapitalPais('');
-            })
-            .catch((error) => {
-                console.error('Error adding country:', error);
-            });
-    }
-
-    return (
-        <View style={Styles.container}> 
-            <View style={Styles.containerTitle}>
-                <Text style={Styles.title}>API- Paises</Text>
-            </View>
-            <View style={Styles.form}>
-                <View style={Styles.contentInput}>
-                    <Text style={Styles.text}>Pais:</Text>
-                    <TextInput 
-                        style={Styles.input}
-                        value={nomePais}
-                        onChangeText={text => setNomePais(text)}
-                    />
-                </View>
-                <View style={Styles.contentInput}>
-                    <Text style={Styles.text}>Capital:</Text>
-                    <TextInput
-                        style={Styles.input}
-                        value={capitalPais}
-                        onChangeText={text => setCapitalPais(text)}
-                    />
-                </View>
-                <TouchableOpacity style={Styles.button} onPress={adicionar} >
-                    <Text style={Styles.buttonText}>ADICIONAR</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={Styles.list}>
-                <Text style={Styles.titleScroll}>Lista de Paises</Text>
-                <FlatList
-                    data={paises}
-                    renderItem={({ item }) => <ListaPaises pais={item} />}
-                    keyExtractor={item => item.id.toString()} // Key deve ser uma string
-                    showsVerticalScrollIndicator={false}
-                />
-            </View>
+  return (
+    <View style={styles.container}> 
+      <View style={styles.containerTitle}>
+        <Text style={styles.title}>API- Paises</Text>
+      </View>
+      <View style={styles.form}>
+        <View style={styles.contentInput}>
+          <Text style={styles.text}>País:</Text>
+          <TextInput 
+            style={styles.input}
+            value={nomePais}
+            onChangeText={text => setNomePais(text)}
+          />
         </View>
-    );
+        <View style={styles.contentInput}>
+          <Text style={styles.text}>Capital:</Text>
+          <TextInput
+            style={styles.input}
+            value={capitalPais}
+            onChangeText={text => setCapitalPais(text)}
+          />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={adicionar} >
+          <Text style={styles.buttonText}>ADICIONAR</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.list}>
+        <Text style={styles.titleScroll}>Lista de Países</Text>
+        <FlatList
+          data={paises}
+          renderItem={({ item }) => <ListaPaises pais={item} atualizaLista={atualizaLista} />}
+          keyExtractor={item => item.id.toString()}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </View>
+  );
 }
--- src/components/paises/listaPaises.js
+++ src/components/paises/listaPaises.js
@@ -1,26 +1,102 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import api from '../service/api.js';
 
-export default function paises({pais}) {
+export default function ListaPaises({ pais, atualizaLista }) {
+  const [id, setId] = useState(null);
+  const [novoNome, setNovoNome] = useState('');
+  const [novaCapital, setNovaCapital] = useState('');
+  const [editando, setEditando] = useState(false); // Estado para controlar se está editando
 
+  useEffect(() => {
+    setId(pais.id);
+    setNovoNome(pais.nome);
+    setNovaCapital(pais.capital);
+  }, [pais]);
 
- return (
+  function deletar() {
+    if (!id) {
+      console.error('ID do país não está definido.');
+      return;
+    }
+
+    api.delete(`/pais/${id}`)
+      .then(response => {
+        Alert.alert('Sucesso', 'O país foi deletado com sucesso.');
+        atualizaLista();
+      })
+      .catch(error => {
+        console.error('Erro ao deletar', error);
+        Alert.alert('Erro', 'Ocorreu um erro ao deletar o país.');
+      });
+  }
+
+  function editar() {
+    api.put(`/pais/${id}`, { nome: novoNome, capital: novaCapital })
+      .then(response => {
+        console.log(response.data);
+        Alert.alert('Sucesso', 'O país foi atualizado com sucesso.');
+        setEditando(false);
+        atualizaLista();
+      })
+      .catch(error => {
+        console.error('Erro ao editar', error);
+        Alert.alert('Erro', 'Ocorreu um erro ao editar o país.');
+      });
+  }
+
+  return (
     <View style={styles.container}>
-        <Text style={styles.text}>Pais: {pais.nome}</Text>
-        <Text style={styles.text}>Capital: {pais.capital}</Text>
+      {editando ? (
+        <View>
+          <TextInput
+            value={novoNome}
+            onChangeText={setNovoNome}
+            style={styles.input}
+          />
+          <TextInput
+            value={novaCapital}
+            onChangeText={setNovaCapital}
+            style={styles.input}
+          />
+          
+          <TouchableOpacity style={styles.botton} onPress={editar}>
+            <Text>Salvar</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <TouchableOpacity onLongPress={deletar} onPress={() => setEditando(true)}>
+          <Text style={styles.text}>País: {pais.nome}</Text>
+          <Text style={styles.text}>Capital: {pais.capital}</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex1: 1,
+const styles = {
+  container: {
+    flex: 1,
     marginBottom: 10,
     backgroundColor: '#f1f1f1',
     padding: 16,
   },
-  text:{
+  text: {
     marginBottom: 6,
     fontSize: 16,
+  },
+  input: {
+    marginBottom: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  botton:{
+    backgroundColor: '#ff0000', 
+    padding: 8,
+    width: '20%', 
+    alignItems: 'center', 
+    borderRadius: 6
   }
-})
+};
